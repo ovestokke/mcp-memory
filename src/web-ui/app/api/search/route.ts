89eb3ffdb0a64ecf5b0cd2daf7321819581d59a2
Kristoffer Remback <kristoffer.remback@telnesstech.com>
2025-08-27 21:30:27 +0200

    To new beginnings!

--- src/web-ui/app/api/search/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from 'next/server'
-import { AuthenticatedWorkerClient } from '../../../lib/worker-client'
-
-export async function POST(request: NextRequest) {
-  const body = await request.json()
-  return AuthenticatedWorkerClient.post('/api/search', body)
-}
